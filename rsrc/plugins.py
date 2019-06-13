10fbb9b561b04f2848f8c20ce73fba34eebdd0fa
Azat Ibrakov <azatibrakov@gmail.com>
2019-06-13 20:12:19 +0300

    Complete 'plugins' module

--- rsrc/plugins.py
+++ rsrc/plugins.py
@@ -6,9 +6,11 @@ from .models import URL
 
 Id = List[str]
 
+MAX_ID_PARTS_COUNT = len(URL._fields)
+
 
 def to_id(*parts: str,
-          max_parts_count: int = len(URL._fields)) -> Id:
+          max_parts_count: int = MAX_ID_PARTS_COUNT) -> Id:
     if len(parts) > max_parts_count:
         raise ValueError('Parts count should not be '
                          'greater than {max_expected_count}, '
@@ -29,7 +31,7 @@ def deserialize_id(string: str) -> Id:
     return json.loads(codecs.decode(string, _ENCODING).decode())
 
 
-def to_location(*, id_: Id, module_name: str, function_name: str) -> str:
+def to_entry_point(*, id_: Id, module_name: str, function_name: str) -> str:
     return ('{id_} = {module_name}:{function_name}'
             .format(id_=serialize_id(id_),
                     module_name=module_name,
